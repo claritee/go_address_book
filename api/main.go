e8012e2d04c5062d1e0affa9d68a12aa4d38d861
Claire Tran <c.tran83@gmail.com>
2020-04-14 16:41:34 +1000

    refactoring structure

---
+++ api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	// "github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("Starting app")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+
+	// r := mux.NewRouter()
+	// r.HandleFunc("/hello", handler).Methods("GET")
+	// http.ListenAndServe(":8080", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
